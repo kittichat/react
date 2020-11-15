e282fc34cbd31dba14e833011b97d6406d40cdac
kittichat <kittichat@gmail.com>
2020-11-15 16:27:32 +0700

    first commit

---
+++ src/Rule.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Rule.css';
+function Rule() {
+    return (
+        <div className="rule">
+            <h1>Rule page</h1>
+        </div>
+    )
+}
+
+export default Rule;
