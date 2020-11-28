c177cdb333deb443bb5bba413bcb5c81820f17cc
kittichat <kittichat@gmail.com>
2020-11-28 15:11:17 +0700

    forth updated

--- src/authen/Register.js
+++ src/authen/Register.js
@@ -1,34 +1,271 @@
-import { InputRounded } from '@material-ui/icons';
 import React from 'react'
+import Form from "react-validation/build/form"
+import Input from "react-validation/build/input"
+import CheckButton from "react-validation/build/button"
+import { isEmail } from 'validator'
+
+import AuthService from '../services/auth_service'
+
 import './../css/Register.css';
 
+const required = value => {
+  if (!value){
+    return (
+      <div className="alert alert-danger" role="alert">
+        This field is requried!
+      </div>
+    );
+  }
+};
+
+const vemail = value =>{
+  if  (!isEmail(value)){
+    return(
+      <div className="alert alert-danger" role="alert">
+        This is not a valid emai;
+      </div>
+    )
+  }
+}
+
+const vusername = value =>{
+  if (value.length < 3 || value.length > 20){
+    return (
+      <div className="alert alert-danger" role="alert">
+        the username must between 3 and 20 characters
+      </div>
+    )
+  }
+}
+
+const vpassword = value => {
+  if (value.length < 6 || value.length > 40){
+    return (
+      <div className="alert alert-danger" role="alert">
+        the password must be between 6 and 40 characters
+      </div>
+    )
+  }
+}
+
+const vconfirmpassword = value =>{
+  if(value.length < 6 || value.length > 40){
+    return (
+      <div className="alert alert-danger" role="alert">
+        the password must be between 6 and 40 characters
+      </div>
+    )
+  }
+}
+
+const vfirstname = value =>{
+  if (value.length === 0 || value.length > 40){
+    return (
+      <div className="alert alert-danger" role="alert">
+        please fill in the firstname and don't let it more than 40
+      </div>
+    )
+  }
+}
+
+const vlastname = value =>{
+  if (value.length === 0 || value.length > 40){
+    return (
+      <div className="alert alert-danger" role="alert">
+        please fill in the lastname and don't let it more than 40
+      </div>
+    )
+  }
+}
 
 
 
-function Register() {
+class Register extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      username:"",
+      firstname:"",
+      lastname:"",
+      passowrd:"",
+      confirmpassword:"",
+      email:"",
+      successful:false,
+    }
+    this.handleRegister = this.handleRegister.bind(this);
+    this.onChangeUsername = this.onChangeUsername.bind(this)
+    this.onChangeFirstname = this.onChangeFirstname.bind(this)
+    this.onChangeLastname = this.onChangeLastname.bind(this)
+    this.onChangePassword = this.onChangePassword.bind(this)
+    this.onChangeConfirmPass = this.onChangeConfirmPass.bind(this)
+    this.onChangeEmail = this.onChangeEmail.bind(this)
+
+  }
+
+  onChangeUsername(e){
+    this.setState({
+      username : e.target.value
+    })
+  }
+
+  onChangeFirstname(e){
+    this.setState({
+      firstname : e.target.value
+    })
+  }
+
+  onChangeLastname(e){
+    this.setState({
+      lastname : e.target.value
+    })
+  }
+
+  onChangePassword(e){
+    this.setState({
+      passowrd : e.target.value
+    })
+  }
+
+  onChangeConfirmPass(e){
+    this.setState({
+      confirmpassword : e.target.value
+    })
+  }
+
+  onChangeEmail(e){
+    this.setState({
+      email : e.target.value
+    })
+  }
+
+
+  handleRegister(e){
+    e.preventDefault()
+
+    this.setState({
+      message: "",
+      successful: false
+    })
+
+    this.form.validateAll();
+
+    if (this.checkBtn.context._error.length === 0 ){
+      AuthService.register(
+        this.state.username,
+        this.state.passowrd,
+        this.state.confirmpassword,
+        this.state.email,
+        
+      ).then(
+        response => {
+          this.setState({
+            message: response.data.message,
+            successful: true
+          });
+        },
+        error => {
+          const resMessage = 
+            (error.response && 
+              error.response.data &&
+              error.response.data.message) ||
+              error.message ||
+              error.toString();
+
+              this.setState({
+                successful: false,
+                message: resMessage
+              })
+        }
+      )
+    }
+  }
+  render(){
   return (  
     <div>
     <div className="Register__bg">
       <h1 className="Register__topic">Register</h1>
-      <form className="Register__form">
+      <Form className="Register__form"
+        onSubmit = {this.handleRegister} 
+         ref = {c => {
+           this.form = c;
+         }}
         
-        <div className="Name">
-          {/* <label className="Firstname" action="" >First name</label> */}
-          <input className="Register__firstname" type="text" placeholder="First name" />
-          {/* <label className="Lastname">Last name</label> */}
-          <input className="Register__lastname" type="text" placeholder="Last name" />
-        </div>
-        {/* <label>Email</label> */}
-        <input className="Register__email" type="text" placeholder="example@email.com" />
+      >
 
-        <input className="Register__username" type="text" placeholder="Username" />
+         {!this.state.successful && (
+          <div>
+            <div className="">
+                <div className="Name">
+                  {/* <label className="Firstname" action="" >First name</label> */}
+                  <Input 
+                    className="Register__firstname" 
+                    type="text"
+                    placeholder="First name" 
+                    name="firstname"
+                    value={this.state.firstname}
+                    onChange={this.onChangeFirstname}
+                    validations={[required, vfirstname]}
+                   />
+                  {/* <label className="Lastname">Last name</label> */}
+                  <Input 
+                    className="Register__lastname" 
+                    type="text"
+                    placeholder="Last name"
+                    value={this.state.lastname}
+                    onChange={this.onChangeLastname}
+                    validations={[required, vlastname]}
+                    />
+                </div>
+                {/* <label>Email</label> */}
+                <Input 
+                  className="Register__email" type="text"
+                  placeholder="example@email.com"
+                  name="email"
+                  value={this.state.email}
+                  onChage={this.onChangeEmail}
+                  validations={[required, vemail]}
+                  />
 
-        <input className="Register__password" type="text" placeholder="Password" />
+                <Input 
+                  className="Register__username" 
+                  type="text"
+                  placeholder="Username"
+                  name="username"
+                  value={this.state.username}
+                  onChangeLastname={this.onChamgeUsername}
+                  validations={[required, vusername]}
+                  />
 
-        <input className="Register__confirmpassword" type="text" placeholder="Confirm Password" />
-        <div className="Register__gender">
-          <label >Gender</label>
-        </div>
+                  <Input 
+                  className="Register__password" 
+                  type="text"
+                  placeholder="Password" 
+                  name="password"
+                  value={this.state.password}
+                  onChange={this.onChangePassword}
+                  validations={[required, vpassword]}
+                  />
+
+                  <Input 
+                  className="Register__confirmpassword" 
+                  type="text"
+                  placeholder="Confirm Password" 
+                  name="confirmpassword"
+                  value={this.state.confirmpassword}
+                  onChange={this.onChangeConfirmPass}
+                  validations={[required, vconfirmpassword]}
+
+                  />
+                
+                <div className="Register__gender">
+                    <label >Gender</label>
+                </div>
+
+            </div>
+          </div>
+        )}
+        
+        {/* not yet handle this place gender and etc */}
 
         <div className="Gender">
           <div className="Checkbox__male">
@@ -44,194 +281,20 @@ function Register() {
         <div className="Register__birthday__label">
           <label >Birthday</label>
         </div>
-        <div className="Register__birthday">
-          <select className="Register__day">
-            <option value="">Day</option>
-            <option value="">---</option>
-            <option value="01">01</option>
-            <option value="02">02</option>
-            <option value="03">03</option>
-            <option value="04">04</option>
-            <option value="05">05</option>
-            <option value="06">06</option>
-            <option value="07">07</option>
-            <option value="08">08</option>
-            <option value="09">09</option>
-            <option value="10">10</option>
-            <option value="11">11</option>
-            <option value="12">12</option>
-            <option value="13">13</option>
-            <option value="14">14</option>
-            <option value="15">15</option>
-            <option value="16">16</option>
-            <option value="17">17</option>
-            <option value="18">18</option>
-            <option value="19">19</option>
-            <option value="20">20</option>
-            <option value="21">21</option>
-            <option value="22">22</option>
-            <option value="23">23</option>
-            <option value="24">24</option>
-            <option value="25">25</option>
-            <option value="26">26</option>
-            <option value="27">27</option>
-            <option value="28">28</option>
-            <option value="29">29</option>
-            <option value="30">30</option>
-            <option value="31">31</option>
-          </select>
+        {/*this space for register component*/}
 
-          <select className="Register__month">
-            <option value="">Month</option>
-            <option value="">-----</option>
-            <option value="01">January</option>
-            <option value="02">February</option>
-            <option value="03">March</option>
-            <option value="04">April</option>
-            <option value="05">May</option>
-            <option value="06">June</option>
-            <option value="07">July</option>
-            <option value="08">August</option>
-            <option value="09">September</option>
-            <option value="10">October</option>
-            <option value="11">November</option>
-            <option value="12">December</option>
-          </select>
 
-          <select className="Register__year">
-            <option value="">Year</option>
-            <option value="">----</option>
-            <option value="2020">2020</option>
-            <option value="2019">2019</option>
-            <option value="2018">2018</option>
-            <option value="2017">2017</option>
-            <option value="2016">2016</option>
-            <option value="2015">2015</option>
-            <option value="2014">2014</option>
-            <option value="2013">2013</option>
-            <option value="2012">2012</option>
-            <option value="2011">2011</option>
-            <option value="2010">2010</option>
-            <option value="2009">2009</option>
-            <option value="2008">2008</option>
-            <option value="2007">2007</option>
-            <option value="2006">2006</option>
-            <option value="2005">2005</option>
-            <option value="2004">2004</option>
-            <option value="2003">2003</option>
-            <option value="2002">2002</option>
-            <option value="2001">2001</option>
-            <option value="2000">2000</option>
-            <option value="1999">1999</option>
-            <option value="1998">1998</option>
-            <option value="1997">1997</option>
-            <option value="1996">1996</option>
-            <option value="1995">1995</option>
-            <option value="1994">1994</option>
-            <option value="1993">1993</option>
-            <option value="1992">1992</option>
-            <option value="1991">1991</option>
-            <option value="1990">1990</option>
-            <option value="1989">1989</option>
-            <option value="1988">1988</option>
-            <option value="1987">1987</option>
-            <option value="1986">1986</option>
-            <option value="1985">1985</option>
-            <option value="1984">1984</option>
-            <option value="1983">1983</option>
-            <option value="1982">1982</option>
-            <option value="1981">1981</option>
-            <option value="1980">1980</option>
-            <option value="1979">1979</option>
-            <option value="1978">1978</option>
-            <option value="1977">1977</option>
-            <option value="1976">1976</option>
-            <option value="1975">1975</option>
-            <option value="1974">1974</option>
-            <option value="1973">1973</option>
-            <option value="1972">1972</option>
-            <option value="1971">1971</option>
-            <option value="1970">1970</option>
-            <option value="1969">1969</option>
-            <option value="1968">1968</option>
-            <option value="1967">1967</option>
-            <option value="1966">1966</option>
-            <option value="1965">1965</option>
-            <option value="1964">1964</option>
-            <option value="1963">1963</option>
-            <option value="1962">1962</option>
-            <option value="1961">1961</option>
-            <option value="1960">1960</option>
-            <option value="1959">1959</option>
-            <option value="1958">1958</option>
-            <option value="1957">1957</option>
-            <option value="1956">1956</option>
-            <option value="1955">1955</option>
-            <option value="1954">1954</option>
-            <option value="1953">1953</option>
-            <option value="1952">1952</option>
-            <option value="1951">1951</option>
-            <option value="1950">1950</option>
-            <option value="1949">1949</option>
-            <option value="1948">1948</option>
-            <option value="1947">1947</option>
-            <option value="1946">1946</option>
-            <option value="1945">1945</option>
-            <option value="1944">1944</option>
-            <option value="1943">1943</option>
-            <option value="1942">1942</option>
-            <option value="1941">1941</option>
-            <option value="1940">1940</option>
-            <option value="1939">1939</option>
-            <option value="1938">1938</option>
-            <option value="1937">1937</option>
-            <option value="1936">1936</option>
-            <option value="1935">1935</option>
-            <option value="1934">1934</option>
-            <option value="1933">1933</option>
-            <option value="1932">1932</option>
-            <option value="1931">1931</option>
-            <option value="1930">1930</option>
-            <option value="1929">1929</option>
-            <option value="1928">1928</option>
-            <option value="1927">1927</option>
-            <option value="1926">1926</option>
-            <option value="1925">1925</option>
-            <option value="1924">1924</option>
-            <option value="1923">1923</option>
-            <option value="1922">1922</option>
-            <option value="1921">1921</option>
-            <option value="1920">1920</option>
-            <option value="1919">1919</option>
-            <option value="1918">1918</option>
-            <option value="1917">1917</option>
-            <option value="1916">1916</option>
-            <option value="1915">1915</option>
-            <option value="1914">1914</option>
-            <option value="1913">1913</option>
-            <option value="1912">1912</option>
-            <option value="1911">1911</option>
-            <option value="1910">1910</option>
-            <option value="1909">1909</option>
-            <option value="1908">1908</option>
-            <option value="1907">1907</option>
-            <option value="1906">1906</option>
-            <option value="1905">1905</option>
-            <option value="1904">1904</option>
-            <option value="1903">1903</option>
-            <option value="1901">1901</option>
-            <option value="1900">1900</option>
-          </select>
-        </div>
         <div className="Register__button__position">
           <button type="submit" className="Register__button__size" >Submit</button>
         </div>
 
-      </form>
+      </Form>
 
     </div>
     </div>
   )
+
+  }
 }
 
 export default Register
