df334ea43082d5107d7fea78ffd964554f7c0d0c
kittichat <kittichat@gmail.com>
2021-05-22 17:15:33 +0700

    First attempt to deploy

---
+++ build/precache-manifest.0ec2e58f3f3d7385a5ea402c42cb8c70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "094f633eb9dc34e3b8700c7b9a3ac655",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "a231308d625397423027",
+    "url": "/react/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "cee0d5716a69f88c6dec",
+    "url": "/react/static/css/main.6541329f.chunk.css"
+  },
+  {
+    "revision": "a231308d625397423027",
+    "url": "/react/static/js/2.5aafbd0b.chunk.js"
+  },
+  {
+    "revision": "56be5b5da09235584aaef7b0c879879a",
+    "url": "/react/static/js/2.5aafbd0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cee0d5716a69f88c6dec",
+    "url": "/react/static/js/main.e88e196d.chunk.js"
+  },
+  {
+    "revision": "ffb0fde1340a1a920e77",
+    "url": "/react/static/js/runtime-main.c907bebd.js"
+  }
+]);
