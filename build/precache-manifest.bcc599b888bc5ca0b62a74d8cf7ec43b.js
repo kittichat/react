1c58aac81611623970846adea4782953aab449c7
kittichat <kittichat@gmail.com>
2021-05-29 18:51:58 +0700

    changed path

---
+++ build/precache-manifest.bcc599b888bc5ca0b62a74d8cf7ec43b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ff57bfbf5d64d9140843d5c5fceed28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6b87f01421510a05000",
+    "url": "/static/css/2.61a2adff.chunk.css"
+  },
+  {
+    "revision": "563e0b9440c63853c8c3",
+    "url": "/static/css/main.87734946.chunk.css"
+  },
+  {
+    "revision": "b6b87f01421510a05000",
+    "url": "/static/js/2.5cc8e1eb.chunk.js"
+  },
+  {
+    "revision": "71632ee1fb2ba019a98fd64a29af12b2",
+    "url": "/static/js/2.5cc8e1eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "563e0b9440c63853c8c3",
+    "url": "/static/js/main.adbf9b75.chunk.js"
+  },
+  {
+    "revision": "16ae9854644794f32750",
+    "url": "/static/js/runtime-main.89434f27.js"
+  }
+]);
